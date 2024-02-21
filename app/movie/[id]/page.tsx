ef37f729b9f3f6d594d783e9296f30ce5c1c3222
Darick <darickmcbride@me.com>
2024-02-20 22:20:39 -0800

    commit

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -27,8 +27,7 @@ export default async function MoviePage({
           width={500}
           height={300}
           alt="Movie Image"
-          className="rounded-lg"
-          style={{ maxWidth: "100%", height: "100%" }}
+          className="rounded-lg max-w-full h-full"
         />
         <div className="p-2 ">
           <h2 className="text-lg mb-3 font-bold">
